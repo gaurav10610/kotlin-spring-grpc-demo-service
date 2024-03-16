d659b2dcb0034e9b384f42148bdd0f65a8f53e1c
Gaurav Kumar Yadav <gaurav10610@gmail.com>
2024-03-16 15:19:51 +0530

    added server streaming method

--- src/main/kotlin/com/example/demo/service/CalculatorService.kt
+++ src/main/kotlin/com/example/demo/service/CalculatorService.kt
@@ -1,8 +1,9 @@
 package com.example.demo.service
 
-import com.example.demo.grpc.CalculatorServiceGrpcKt
-import com.example.demo.grpc.OperationRequest
-import com.example.demo.grpc.OperationResponse
+import com.example.demo.grpc.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flow
 import org.springframework.stereotype.Service
 
 @Service
@@ -21,5 +22,27 @@ class CalculatorService : CalculatorServiceGrpcKt.CalculatorServiceCoroutineImpl
 
     override suspend fun division(request: OperationRequest): OperationResponse {
         return OperationResponse.newBuilder().setResult(request.x / request.y).build();
+    }
+
+    override fun generatePrimeNumbers(request: PrimeNumberRequest): Flow<PrimeNumberResponse> {
+        return flow {
+            for (x in 2..request.number) {
+                if (isPrime(x)) {
+                    emit(PrimeNumberResponse.newBuilder().setPrime(x).build())
+                }
+            }
+        }
+    }
+
+    private fun isPrime(n: Int): Boolean {
+        if (n <= 1) return false
+        if (n == 2 || n == 3) return true
+        if (n % 2 == 0 || n % 3 == 0) return false
+        var i = 5
+        while (i * i <= n) {
+            if (n % i == 0 || n % (i + 2) == 0) return false
+            i += 6
+        }
+        return true
     }
 }
