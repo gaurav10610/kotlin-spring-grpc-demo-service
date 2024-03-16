bf8adff9ed412d6f2153cf3d4dad99bcd550ea87
Gaurav Kumar Yadav <gaurav10610@gmail.com>
2024-03-16 13:54:17 +0530

    minor change

--- src/main/kotlin/com/example/demo/service/HelloService.kt
+++ src/main/kotlin/com/example/demo/service/HelloService.kt
@@ -1,6 +1,5 @@
 package com.example.demo.service
 
-import com.example.demo.configuration.GrpcServerConfig
 import com.example.demo.grpc.HelloReply
 import com.example.demo.grpc.HelloRequest
 import com.example.demo.grpc.HelloServiceGrpcKt
